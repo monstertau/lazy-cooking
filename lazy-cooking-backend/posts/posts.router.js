3abe7471425c804a514e20cada618aabc736abcc
letrongtri99 <trichuot99@gmail.com>
2019-08-27 11:01:23 +0700

    tri

--- lazy-cooking-backend/posts/posts.router.js
+++ lazy-cooking-backend/posts/posts.router.js
@@ -4,6 +4,15 @@ const postRouter = express.Router();
 
 postRouter.post(`/create`,(req,res)=>{
     const {content,title,imageUrl,category,materials,level,timetodone} = req.body;
+    const post ={
+        content: req.body.content,
+        title: req.body.title,
+        imageUrl:req.body.imageUrl,
+        category: req.body.category,
+        materials: req.body.materials,
+        level: req.body.level,
+        timetodone: req.body.timetodone
+    }
     if(title.length >30 || content.length >1000){
         res.status(400).json({
             success: false,
@@ -11,7 +20,7 @@ postRouter.post(`/create`,(req,res)=>{
         });
     }
     else{
-        postModel.find({title:title},(error,data)=>{
+        postModel.findOne({title:title},(error,data)=>{
             if (error) {
                 res.status(500).json({
                     success: false,
@@ -25,22 +34,72 @@ postRouter.post(`/create`,(req,res)=>{
                 })
             }
             else{
-                postModel.create(...req.body,(error2,data2)=>{
+                postModel.create(post,(error1,data1)=>{
                     if (error1) {
                         res.status(500).json({
                             success: false,
-                            message: error2.message,
+                            message: error.message,
                         })
                     }
                     else {
                         res.status(201).json({
                             success: true,
-                            data: data2
+                            data: data1
                         });
                     }
                 })
             }
         })
+    }
+})
+
+postRouter.get(`/getpost`, (req, res) => {
+    //sorBy:price
+    //sortOrder:a-z
+    const pageNumber = Number(req.query.pageNumber);
+    const pageSize = 10;
+    if (isNaN(pageNumber) ) {
+        res.status(500).json({
+            success: false,
+            message: 'pageNumber are invalid'
+        })
+    }
+    else if (pageNumber < 1 || pageSize < 1 || pageSize > 20) {
+        res.status(500).json({
+            success: false,
+            message: 'pageNumber and pageSize are invalid'
+        })
+    }
+    else {
+        postModel.find({})
+            .sort({ createAt: -1 })
+            .skip(pageSize * (pageNumber - 1))
+            .limit(pageSize)
+            .exec((error, data) => {
+                if(error){
+                    res.status(500).json({
+                        success: false,
+                        message: error.message
+                    })
+                }
+                else{
+                    postModel.find({}).countDocuments().exec((error,total)=>{
+                        if(error){
+                            res.status(500).json({
+                                success: false,
+                                message: error.message
+                            })
+                        }
+                        else{
+                            res.status(200).json({
+                                success:true,
+                                data:data,
+                                total:total
+                            })
+                        }
+                    })
+                }
+            })
     }
 })
 
