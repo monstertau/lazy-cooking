d0f65ae34c9a854d83cbf91500736b3f1fbf1df8
letrongtri99 <trichuot99@gmail.com>
2019-09-01 10:46:03 +0700

    tri

--- lazy-cooking-backend/users/users.model.js
+++ lazy-cooking-backend/users/users.model.js
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
     phone: {
-        type: Number,
+        type: String,
         required: true,
     },
     avatarUrl: {
